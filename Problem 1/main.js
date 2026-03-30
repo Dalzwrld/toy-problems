f844842e53d6460d4d2178473293cc83e2d5331f
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-03-30 18:20:03 +0300

    feat: add main js file

--- Problem 1/main.js
+++ Problem 1/main.js
@@ -3,13 +3,26 @@
 // 1. A function that takes a positive integer and returns the next smaller positive integer containing the same digits.
 
 function nextSmaller(num) {
-    let numStr = num.toString();
+    let numStr = num.toString().split("");
 
-    for (let i = 0; i < numStr.length; i++) {
-        const element = numStr[i];
-        
+    for (let i = numStr.length - 2; i >= 0; i--) {
+        if (numStr[i] > numStr[i + 1]) {
+            let j = numStr.length - 1;
+            while (numStr[j] >= numStr[i]) {
+                j--;
+            }
+            [numStr[i], numStr[j]] = [numStr[j], numStr[i]];
+            return parseInt(numStr.slice(0, i + 1).concat(numStr.slice(i + 1).reverse()).join(""));
+        }
     }
+    return -1; // Return -1 if no smaller number can be formed
 }
+
+console.log(nextSmaller(123));
+console.log(nextSmaller(321));
+console.log(nextSmaller(21));
+console.log(nextSmaller(531));
+console.log(nextSmaller(2071));
 
 
 // 2. A function that returns the largest of three given variables.
