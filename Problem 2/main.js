661284638d708b4b32c156d06d3958966a520c2c
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-08 09:54:19 +0300

    feat: add main js file

--- Problem 2/main.js
+++ Problem 2/main.js
@@ -2,8 +2,9 @@
 
 // 1. GIVEN AN ARRAY OF INTEGERS, FIND THE LARGEST ELEMENT.
 
-const arr = [3, 7, 2, 9, 5];
+const numbers = [3, 7, 2, 9, 5];
+
+const [num1, num2, num3, num4, num5] = arr;
 
 function largestElement(num) {
-    let numStr = num.toString().split("");
 }
