9c565f20f582cc04ade6886e42b2a4b2ffec43cd
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-08 10:50:17 +0300

    feat: add main js file

--- Problem 2/main.js
+++ Problem 2/main.js
@@ -9,3 +9,18 @@ const max = Math.max(...numbers);
 const largestNumber = numbers.filter(num => num === max);
 
 console.log(largestNumber[0]);
+
+
+// 2. GIVEN AN ARRAY, CHECK WHETHER IT'S SORTED IN A NON-DECREASING MANNER
+
+const array = [1, 2, 3, 4, 5];
+
+function sortedArray(array) {
+    for (let i = 0; i < array.length -1; i++) {
+        if (array[i] > array[i + 1]) {
+            return false;
+        }
+    } return true;
+}
+
+console.log();
