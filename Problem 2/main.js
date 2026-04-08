8308b4d457c8fdffb4a3d54762730f3848ab86bd
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-08 09:39:24 +0300

    feat: add main js file

--- Problem 2/main.js
+++ Problem 2/main.js
@@ -1,3 +1,9 @@
-// 1. Given an array of integers, find the largest element.
+// 8/4/2026
+
+// 1. GIVEN AN ARRAY OF INTEGERS, FIND THE LARGEST ELEMENT.
 
 const arr = [3, 7, 2, 9, 5];
+
+function largestElement(num) {
+    let numStr = num.toString().split();
+}
